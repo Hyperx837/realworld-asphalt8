615ceef6879b4f7ad4836cbed881c52f4c98170d
Anupama Dissanayake <61502343+Anu2001dev@users.noreply.github.com>
2021-01-25 11:15:15 +0530

    setup pins and digital read buttons ready 🔘

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pyfirmata import util
 from pynput.keyboard import Controller, Key
 
 from utils import ArduinoNano, delay
@@ -5,13 +6,19 @@ from utils import ArduinoNano, delay
 keyboard = Controller()
 board = ArduinoNano('/dev/ttyUSB0')
 
-it =
+# start iterator to read pins without blocking main thread
+it = util.Iterator()
+it.start()
 
-nitro_button = board.get_pin('d:2:i')
+# pin setup
+nitro_btn = board.get_pin('d:2:i')
+accel_btn = board.get_pin('d:3:i')
+brake_btn = board.get_pin('d:4:i')
 
 
 def loop():
-    pass
+    if nitro_btn.read():
+        print('hi')
 
 
 if __name__ == '__main__':
